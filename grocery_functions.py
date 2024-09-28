3325d536d24d17ca818e0e73d2ce5dc59296c6c4
itasari <itasari@gmail.com>
2024-09-28 15:54:56 -0700

    clean up grocery_functions.py

--- grocery_functions.py
+++ grocery_functions.py
@@ -1,89 +1,79 @@
+import json
 import os
 import requests
-from langfuse.decorators import observe
-from oauthlib.oauth2 import BackendApplicationClient
-from requests_oauthlib import OAuth2Session
-from requests.auth import HTTPBasicAuth
-import json
-import base64
-
 from dotenv import load_dotenv
+from langfuse.decorators import observe
+from requests.auth import HTTPBasicAuth
 
-# for testing
+# for testing - temp until we have a better way to get location id (ticket #3)
 GROCERY_LOCATION_IDS = {
     "Bellevue-QFC": "70500822",
     "Vancouver-FredMeyer": "70100140",
 }
 
-@observe()
-def get_access_token():
-    load_dotenv()
+API_SCOPE = {
+    "product": "product.compact"
+}
 
+@observe()
+def get_access_token(scope=None):
+    load_dotenv()
     client_id = os.getenv('KROGER_CLIENT_ID')
     client_secret = os.getenv('KROGER_CLIENT_SECRET')
     token_url = os.getenv('KROGER_TOKEN_ENDPOINT')
+    auth = HTTPBasicAuth(client_id, client_secret)
+
+    if scope is None:
+        data = {'grant_type': 'client_credentials'}
+    else:
+        data = {'grant_type': 'client_credentials', 'scope': scope}
+
+    response = requests.post(token_url, auth=auth, data=data)
+
+    if response.status_code != 200:
+        raise Exception(f"Error: Failed to get access token: {response.status_code}")
     
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': f'Basic {base64.b64encode(f"{os.getenv('KROGER_CLIENT_ID')}:{os.getenv('KROGER_CLIENT_SECRET')}".encode()).decode()}'
-    }
-    data = 'grant_type=client_credentials&scope=product.compact'
-    token= json.loads(requests.post(token_url, headers=headers, data=data).text)
-    return token['access_token']
-    
-
-
-    # auth = HTTPBasicAuth(client_id, client_secret)
-    # client = BackendApplicationClient(client_id=client_id)
-    # oauth = OAuth2Session(client=client)
-    # token = oauth.fetch_token(token_url=token_url, client_id=client_id, client_secret=client_secret)
-    
-    # return token['access_token']
-
-print(get_access_token())
-
-def generate_request():
-    
-
-    url = f"https://api.kroger.com/v1/connect/oauth2/token"
-    headers = {
-        "accept": "application/json",
-        "Authorization": f"Bearer {os.getenv('TMDB_API_ACCESS_TOKEN')}"
-    }
-
-    response = requests.get(url, headers=headers)
-    cart_data = response.json()
-    return cart_data
+    try:
+        token_data = response.json()
+        if 'access_token' in token_data:
+            return token_data['access_token']
+        else:
+            raise Exception(f"Error: Access token not found in response: {token_data}")
+    except json.JSONDecodeError:
+        raise Exception(f"Error: Failed to decode JSON {response.text}")
 
 @observe()
-def get_grocery_items(limit=5, location_id=None):
-    # url = f"https://api.kroger.com/v1/locations/{location_id}/cart/items"
-    # headers = {
-    #     "accept": "application/json",
-    #     "Authorization": f"Bearer {os.getenv('TMDB_API_ACCESS_TOKEN')}"
-    # }
-
-    # response = requests.get(url, headers=headers)
-    # cart_data = response.json()
-    # return cart_data
-    #return "Strawberries, Celery, Apples, Carrots, Onions"
-
-    access_token = get_access_token()
-    url = 'https://api.kroger.com/v1/products?filter.term=produce&filter.locationId=01400943&filter.fulfillment=csp&filter.limit=10'
+def get_grocery_items(location_id, limit=10):
+    try:
+        access_token = get_access_token(scope=API_SCOPE['product'])
+    except Exception as e:
+        return f"Error: Failed to get access token: {e}, cannot get grocery items"
+    
+    url = f'https://api.kroger.com/v1/products'
+    params = {
+        'filter.term': 'produce',
+        'filter.locationId': location_id,
+        'filter.fulfillment': 'csp',
+        'filter.limit': limit
+    }
     headers = {
-        'accept': 'application/json',
+        'Accept': 'application/json',
         'Authorization': f'Bearer {access_token}'
     }
-    response = json.loads(requests.get(url, headers=headers).text)
-    descriptions = list(map(lambda item: item['description'], response['data']))
-    # response_items = response['data']
-    # return response_items
-    #print(response)
-    return descriptions
 
-print(get_grocery_items())
+    response = requests.get(url, params=params, headers=headers)
+    if response.status_code != 200:
+        return f"Error: Failed to get grocery items: {response.status_code}"
 
+    try:
+        response_data = response.json()
+        if 'data' in response_data:
+            return [item['description'] for item in response_data['data']]
+        else:
+            print(f"Error: No data found in response {response_data}")
+            return "No grocery items found in the nearby grocery stores"
+    except Exception as e:
+        print(f"Error: Failed to get grocery items: {e}")
+        return "No grocery items found in the nearby grocery stores"
 
-
-
-
+print(get_grocery_items(limit=20, location_id=GROCERY_LOCATION_IDS['Bellevue-QFC']))
