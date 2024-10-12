353a383289ea0e33f58ad21259965896165e5d0e
itasari <itasari@gmail.com>
2024-10-11 22:01:39 -0700

    copy user_info to prompt, update rag_pipeline

--- prompts.py
+++ prompts.py
@@ -1,13 +1,35 @@
-SYSTEM_PROMPT = """
+USER_INFO = """
+## Client Information
+**Name:** Johnny Danger
+**Age:** 30
+**Zipcode:** 92105
+
+## Client Questionnaire
+- **What is your goal?:** My goal is to eat healthier, more vegetables, get more fiber in my diet.
+- **Daily macros/calories in mind, if any?:** Protein 80g, Fiber 30g
+- **What staple foods do you already have around?:** Rice, pasta, oil
+- **How much do you weigh?:** 150lbs
+- **How tall are you?:** 5'8”
+- **What you usually eat for breakfast?:** 1 cup oatmeal, 1/2 cup blueberries, 1/2 cup almond milk, 2 eggs
+- **What's your gender?:** Male
+- **Any dietary restrictions/food allergies/intolerances/personal preferences & cuisines?:** Allergies to fish and eggs
+- **How many are you cooking for?:** 4 people
+"""
+
+SYSTEM_PROMPT = f"""
 You are a nutritionist tasked with helping a client meet their nutritional goals.  You will focus on changing
 their dinner habits.
 
 Your main daily task is to help the client decide what to make for dinner.
 
-You will use the following guidelines to help them decide to have for dinner. Your response should be simple,
-friendly, and welcoming, but not verbose. It is preferrable to guide them step by step instead of asking a lengthy list of questions.
+Client personal information is as described here:
+{USER_INFO}
 
-1. are they cooking at home or eating out?
+In general, your response MUST be simple, friendly, and welcoming, and AVOID being verbose. It is preferrable to guide them step by
+step instead of asking a lengthy list of questions. Please include nutritional information in your response.
+
+You will use the following guidelines to help them decide what to have for dinner.
+1. Before making a suggestion, you should find out if they cooking at home or eating out?
 
 If they are cooking at home, you will use the following guidelines to help them decide what to make for dinner.
 - what ingredients they have on hand
--- rag_pipeline.py
+++ rag_pipeline.py
@@ -27,25 +27,16 @@ class RAGPipeline:
         query_engine = index.as_query_engine()
 
         query_str = """
-            Provide a comprehensive synopsis of the user's important information.  This will be used
-            to provide context when interacting with the user. 
-        """
-        
-        response = query_engine.query(query_str)
-        print(f"response.response: {response.response}")
-        user_summary = response.response
-
-        query_str = """
-            Provide a a summary of any relevent information from the user's previous interactions with
-            the system from the alerts, meal preferences, and chat records. 
+            Provide a summary of any relevant information from the user's previous interactions with
+            the system with regards to their meal preferences.
         """
 
         response = query_engine.query(query_str)
-        print(f"response.response: {response.response}")
-        user_interactions_summary = response.response
+        print(f"response.response meal preferences: {response.response}")
+        user_meal_preferences_summary = response.response
 
         formatted_response = f"""
-        CONTEXT: This is the user's information as provided by the RAG pipeline: {user_summary + user_interactions_summary}. 
+        CONTEXT: This is the user's information as provided by the RAG pipeline: {user_meal_preferences_summary}.
         You should use this information as additional context when responding to the user's query.
         """
         return formatted_response
